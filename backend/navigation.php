ca77f286361a44247a9918cb8d9f825befcbb0c2
HenrikOelze <h.oelze@wemake-it.de>
2011-08-25 16:32:58 +0200

    offene bestellungen

---
+++ backend/navigation.php
@@ -0,0 +1,7 @@
+<br />
+<a href="index.php">Home ;)</a><br /><br />
+<a href="index.php?site=belag"> Belaege</a><br />
+<a href="index.php?site=belagkat"> Belag- Kategorien</a><br />
+<a href="index.php?site=produkt">Produkte</a><br />
+<a href="index.php?site=size">Size</a><br />
+<a href="index.php?site=test">Test</a><br />
--- backend/overview.php
+++ backend/overview.php
@@ -2,3 +2,40 @@
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>und links neben mich eine kleine backend navigation ;)</p>
+
+<?php
+if(isset($_GET['action'])){
+	
+if ($_GET['action'] = 'show'){
+	$BestellID = $_GET['ID'];
+	
+	// dazugehoerige Items auflisten 
+	// bestellung "fertigstellen" 
+}
+
+
+}
+else {
+	$offeneBestellungenq = mysql_query("SELECT * FROM bestellung WHERE done=0");// ID; kunde_ID; datum; wish; done=0
+	
+	
+	while($offeneBestellungen = mysql_fetch_array($offeneBestellungenq)){
+		
+		$kunden_infoq = mysql_query("SELECT * FROM kunde WHERE ID=".$offeneBestellungen['kunde_ID'].""); //ID; anrede; vorname; nachname; strasse; hausnummer; plz; stadt; telefon
+		$kunden_info = mysql_fetch_array($kunden_infoq);
+		echo'
+		<table border="0">
+	  <tr>
+		<td>Offene Bestellung f&uuml;r:</td>
+		<td>'.$kunden_info['anrede'].'</td>
+		<td>'.$kunden_info['vorname'].', '.$kunden_info['nachname'].'</td>
+		
+		<td><a href=index.php?site=overview&action=show&ID='.$offeneBestellungen['kunde_ID'].'> Bestellung einsehen</a></td>
+	  </tr>
+	</table>';
+		
+		
+	}
+}
+?>
+
