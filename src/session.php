f92319e66b204dfb21176056fb132365e82775cf
HenrikOelze <h.oelze@wemake-it.de>
2011-08-29 22:11:09 +0200

    Signed-off-by: HenrikOelze <h.oelze@wemake-it.de> ein Jahrzehnte altes cookie? ;)

--- src/session.php
+++ src/session.php
@@ -8,7 +8,9 @@ if(!isset($_COOKIE['kunde_ID'])){
   if(isset($_GET['site'])){
     if($_GET['site'] == 'order'){
       if(isset($_POST['send'])){
-        $t = time() + 60 * 60 * 24 * 365 * 10; // Cookie ist 10 Jahre haltbar
+        $t = time() + 60 * 60 * 24 * 365 * 10; // Cookie ist 10 Jahre haltbar  
+		
+		// ein 10 - Jahres Cookie ?! WTF? ;)
         setcookie("kunde_ID", $_SESSION['ID'], $t);
       }
     }
