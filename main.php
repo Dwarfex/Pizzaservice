ab3b2247ad947d8e284d981d24f38669476ec27e
HenrikOelze <h.oelze@wemake-it.de>
2011-08-29 16:51:28 +0200

    main.php - Begruessungstext :D Signed-off-by: HenrikOelze <h.oelze@wemake-it.de>

--- main.php
+++ main.php
@@ -1,1 +1,6 @@
-main content
+<h1>Willkommen in der Familie</h1><br /><br />
+Diese Seite ensteht im Rahmen des Kurses PRG der HAW (Sommersemester 2011). <br /><br />
+
+Nur zu demonstrations Zwecken! Keine Bearbeitung der Bestellungen! ;)<br /><br />
+
+Mfg Listener 
