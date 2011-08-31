4c82355bbe2e8626ff8bfc9c4f02e6d842633f96
Martin Trapp <hh-amburg1985@web.de>
2011-08-31 16:38:34 +0200

    run run

--- backend/overview.php
+++ backend/overview.php
@@ -57,7 +57,7 @@ if(isset($_GET['action'])){
                                 FROM produktzubestellung, produkt, produktpreis, size
                                 WHERE produktzubestellung.bestell_ID = '".$_GET['ID']."'
                                 AND produktzubestellung.produkt_ID = produkt.ID
-                                AND Produkt.ID = produktpreis.produkt_ID
+                                AND produkt.ID = produktpreis.produkt_ID
                                 AND produktpreis.size = size.size
                                 AND size.size = produktzubestellung.size
                                 AND size.size = produktpreis.size
